de221f8de682b1a0f1cfbff4bba942cfc3e7ce64
Carlo <carlo.dri@gmail.com>
2023-12-12 22:40:36 +0100

    try to add win64 build with ninja

---
+++ recipe/bld.bat
@@ -0,0 +1,23 @@
+:: Setup directory structure
+mkdir build
+if errorlevel 1 exit 1
+
+:: Configure 
+cmake -G "Ninja" -B "build" -S . ^
+         -DCMAKE_BUILD_TYPE=Release ^
+         -DCMAKE_C_COMPILER=cl ^
+         -DCMAKE_CXX_COMPILER=cl ^
+         -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+         -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+         -DCMAKE_VERBOSE_MAKEFILE=ON ^
+         -DUSE_SHARED_MBEDTLS_LIBRARY=ON ^
+         -DENABLE_TESTING=Off
+if errorlevel 1 exit 1
+
+:: Build and install
+cd build
+if errorlevel 1 exit 1
+ninja
+if errorlevel 1 exit 1
+ninja install
+if errorlevel 1 exit 1
